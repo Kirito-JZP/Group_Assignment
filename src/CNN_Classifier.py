789b15b9ee5d2c6169e9618987ea696816c614cf
zguo <zguo@tcd.ie>
2021-12-05 00:18:51 +0000

    update CNN_Classifier.py

--- src/CNN_Classifier.py
+++ src/CNN_Classifier.py
@@ -11,53 +11,59 @@ from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 import sys
 import ReadImage as RI
+import datetime
 
 
-x_train, y_train, x_test, y_test = RI.make_dataset()
-y_train = keras.utils.to_categorical(y_train, 2)
-y_test = keras.utils.to_categorical(y_test, 2)
+if __name__ == '__main__':
+    ts = datetime.datetime.now()
+    x_train, y_train, x_test, y_test = RI.make_dataset()
+    y_train = keras.utils.to_categorical(y_train, 2)
+    y_test = keras.utils.to_categorical(y_test, 2)
 
-print(y_train)
+    print(y_train)
 
-#Train model
-use_saved_model = False
-if use_saved_model:
-    model = keras.models.load_model("CNN.model")
-else:
-    model = keras.Sequential()
-    model.add(Conv2D(8, (3, 3), padding='same', input_shape=(48, 48, 1), activation='relu'))
-    model.add(Conv2D(8, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+    #Train model
+    use_saved_model = False
+    if use_saved_model:
+        model = keras.models.load_model("CNN.model")
+    else:
+        model = keras.Sequential()
+        model.add(Conv2D(8, (3, 3), padding='same', input_shape=(48, 48, 1), activation='relu'))
+        model.add(Conv2D(8, (3, 3), padding='same', activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
 
-    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+        model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
+        model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
 
-    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+        model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+        model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
 
-    model.add(Dropout(0.5))
-    model.add(Flatten())
-    model.add(Dense(2,activation='softmax', kernel_regularizer=regularizers.l1(0.0001)))
-    model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
-    model.summary()
+        model.add(Dropout(0.5))
+        model.add(Flatten())
+        model.add(Dense(2,activation='softmax', kernel_regularizer=regularizers.l1(0.0001)))
+        model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
+        model.summary()
 
-    batch_size = 64
-    epochs = 20
-    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
-    model.save("CNN.model")
-    plt.subplot(121)
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'val'], loc='upper left')
-    plt.subplot(122)
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss'); plt.xlabel('epoch')
-    plt.legend(['train', 'val'], loc='upper left')
-    plt.show()
+        batch_size = 64
+        epochs = 20
+        history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
+        model.save("CNN.model")
+        te = datetime.datetime.now()
+        print('time elapsed: {}s'.format((te - ts).total_seconds()))
+        
+        plt.subplot(121)
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper left')
+        plt.subplot(122)
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss'); plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper left')
+        plt.show()
