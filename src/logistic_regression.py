b97051e3b533951af5cb595cb3c182beffe4c5df
Zehua Guo <zguo@tcd.ie>
2021-12-06 18:08:25 +0000

    update logistic_regression.py

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -1,12 +1,14 @@
+from operator import mod
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
-from sklearn.model_selection import KFold, cross_val_score
-from sklearn.metrics import confusion_matrix, roc_curve, f1_score
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix, roc_curve, f1_score, log_loss
 from scikitplot.metrics import plot_confusion_matrix
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
 import os
+import datetime
+from PIL import Image
 
 
 def train_lr(x, y, C=1, penalty='none', solver='lbfgs'):
@@ -43,17 +45,28 @@ def convert_to_vector(imgs):
         ret.append(img.reshape(img.size))
     return np.array(ret)
 
+def get_pca():
+    Children_train = "Image/Children_train"
+    Adults_train = "Image/Adults_train"
+    x_children_train = read_img_batch(Children_train)
+    x_adults_train = read_img_batch(Adults_train)
+    x_train = np.array(x_children_train + x_adults_train)
+    pca = PCA(917)
+    pca.fit(convert_to_vector(x_train))
+    return pca
 
 def find_dimension(imgs):
     vectorised_x_train = convert_to_vector(imgs)
     n_components = 1
     while True:
+        print(n_components)
         pca = PCA(n_components)
         pca.fit_transform(vectorised_x_train)
         flag = False
         if pca.explained_variance_ratio_.sum() > 0.99:
             n_components -= 100
             while True:
+                print(n_components)
                 pca = PCA(n_components)
                 pca.fit_transform(vectorised_x_train)
                 if pca.explained_variance_ratio_.sum() > 0.99:
@@ -67,8 +80,42 @@ def find_dimension(imgs):
             n_components += 100
     return n_components
 
+def get_plot_data():
+    Children_test = "Image/Children_test"
+    Children_train = "Image/Children_train"
+    Adults_test = "Image/Adults_test"
+    Adults_train = "Image/Adults_train"
+
+    # read image from each group
+    x_children_train = read_img_batch(Children_train)
+    x_children_test = read_img_batch(Children_test)
+    x_adults_train = read_img_batch(Adults_train)
+    x_adults_test = read_img_batch(Adults_test)
+
+    # set label according to each image set
+    # children 0; adults 1
+    y_children_train = np.zeros(len(x_children_train), dtype=int)
+    y_children_test = np.zeros(len(x_children_test), dtype=int)
+    y_adults_train = np.ones(len(x_adults_train), dtype=int)
+    y_adults_test = np.ones(len(x_adults_test), dtype=int)
+
+     # combine training set and testing set
+    x_train = np.array(x_children_train + x_adults_train)
+    y_train = np.append(y_children_train, y_adults_train)
+    x_test = np.array(x_children_test + x_adults_test)
+    y_test = np.append(y_children_test, y_adults_test)
+
+    pca = PCA(917)
+    pca.fit(convert_to_vector(x_train))
+
+    reduced_x_train = pca.fit_transform(convert_to_vector(x_train))
+    model = train_lr(reduced_x_train, y_train.ravel())
+    fpr, tpr, _ = roc_curve(y_test, model.decision_function(pca.transform(convert_to_vector(x_test))))
+
+    return (fpr, tpr)
 
 if __name__ == '__main__':
+    ts = datetime.datetime.now()
     Children_test = "Image/Children_test"
     Children_train = "Image/Children_train"
     Adults_test = "Image/Adults_test"
@@ -93,68 +140,42 @@ if __name__ == '__main__':
     x_test = np.array(x_children_test + x_adults_test)
     y_test = np.append(y_children_test, y_adults_test)
 
-    model = None
-    pca = None
-    from_beginning = False
-    if from_beginning:
-        n_components = find_dimension(x_train)
-        pca = PCA(n_components)
-        reduced_x_train = pca.fit_transform(convert_to_vector(x_train))
-        print(reduced_x_train.shape)
-        print(convert_to_vector(x_train).shape)
-
-        c_values = [0.1, 1, 10, 100, 1000]
-        scores = []
-        std_devs = []
-        models = {}
-        for c in c_values:
-            model = train_lr(reduced_x_train, y_train.ravel(), C=c, penalty='l1')
-            mean_score, std_dev = cross_validation(model, reduced_x_train, y_train.ravel(), cv=5)
-            scores.append(mean_score)
-            std_devs.append(std_dev)
-            models[c] = model
-
-        model_without_penalty = train_lr(reduced_x_train, y_train.ravel())
-        model_score = f1_score(y_test.ravel(), model_without_penalty.predict(pca.transform(convert_to_vector(x_test))))
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1)
-        ax.set_title("Model Selection against $C$")
-        ax.set_xlabel(r"$C$")
-        ax.set_ylabel("F1 score")
-        ax.plot(c_values, [model_score] * len(c_values), label='model without penalty term')
-        ax.errorbar(c_values, scores, yerr=std_devs, label='model with l2 penalty term')
-        ax.legend()
-        plt.show()
-
-        model = models[1]
-    else:
-        pca = PCA(917)
-        reduced_x_train = pca.fit_transform(convert_to_vector(x_train))
-        model = train_lr(reduced_x_train, y_train.ravel())
-        score = f1_score(y_test.ravel(), model.predict(pca.transform(convert_to_vector(x_test))))
-
+    # find #component
+    n_components = find_dimension(x_train)
+    pca = PCA(n_components)
+    reduced_x_train = pca.fit_transform(convert_to_vector(x_train))
+    model = train_lr(reduced_x_train, y_train.ravel())
+    score = f1_score(y_test.ravel(), model.predict(pca.transform(convert_to_vector(x_test))))
+    # evaluation on training set
     pred = model.predict(reduced_x_train)
     cm_lr = confusion_matrix(y_train.ravel(), model.predict(pca.transform(convert_to_vector(x_train))))
     tn, fp, fn, tp = cm_lr.ravel()
-
     print("(on training set)")
     print("tn: {}, fp: {}, fn: {}, tp: {}".format(tn, fp, fn, tp))
     print("accuracy: {}".format((tn + tp) / (tn + tp + fn + fp)))
     print("f1 score: {}".format(2 * tp / (2 * tp +fn + fp)))
+    print("cross-entropy loss: {}".format(log_loss(y_train, model.predict(pca.transform(convert_to_vector(x_train))))))
+    # evaluation on test set
     pred = model.predict(pca.transform(convert_to_vector(x_test)))
     cm_lr = confusion_matrix(y_test.ravel(), model.predict(pca.transform(convert_to_vector(x_test))))
     tn, fp, fn, tp = cm_lr.ravel()
-
     print("(on test set)")
     print("tn: {}, fp: {}, fn: {}, tp: {}".format(tn, fp, fn, tp))
     print("accuracy: {}".format((tn + tp) / (tn + tp + fn + fp)))
     print("f1 score: {}".format(2 * tp / (2 * tp +fn + fp)))
+    y_cat_0 = y_test
+    y_cat_1 = np.abs(y_test - 1)
+    y_test_one_hot = np.column_stack((y_cat_0, y_cat_1))
+    print("cross-entropy loss: {}".format(log_loss(y_test_one_hot, model.predict_proba(pca.transform(convert_to_vector(x_test))))))
     plot_confusion_matrix(y_test.ravel(), pred.ravel())
-    
+    # ROC curve on test set
     fpr, tpr, _ = roc_curve(y_test, model.decision_function(pca.transform(convert_to_vector(x_test))))
     plt.figure()
     plt.plot(fpr, tpr)
     plt.xlabel('False positive rate')
     plt.ylabel('True positive rate')
     plt.plot([0, 1], [0, 1], color='green', linestyle='--')
+    te = datetime.datetime.now()
+    print('time elapsed: {}s'.format((te - ts).total_seconds()))
+
     plt.show()
